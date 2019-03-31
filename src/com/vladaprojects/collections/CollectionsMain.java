c4035cc0b0f54861641863f9943a8eb3edd04d55
VladaKmets <vlada.zakomorna@gmail.com>
2019-03-31 19:41:52 +0300

    add maven

--- src/com/vladaprojects/collections/CollectionsMain.java
+++
@@ -1,5 +0,0 @@
-package com.vladaprojects.collections;
-
-public class CollectionsMain {
-
-}
--- src/com/vladaprojects/films/services/exception/FilmNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.vladaprojects.films.services.exception;
-
-public class FilmNotFoundException extends Exception {
-
-    public FilmNotFoundException(String message) {
-        super(message);
-    }
-}
